fe54f2eb30db8bbb9870d0ce71c5648f548579d5
HoNhutHao <lthao2212@gmail.com>
2025-09-18 19:44:24 +0700

    update2

--- app.js
+++ app.js
@@ -35,5 +35,7 @@ app.get('/suppliers', async (req, res) => {
     res.render('suppliers/index', { suppliers });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
